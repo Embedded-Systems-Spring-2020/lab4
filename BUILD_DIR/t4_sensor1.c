c4e0ebd91c8219cf449fe6728b70877bbbf9982e
Ryan Shoemake <ras603@msstate.edu>
2020-02-21 18:38:11 -0600

    Add pot example and task 1 file

---
+++ BUILD_DIR/t4_sensor1.c
@@ -0,0 +1,15 @@
+#include <esos.h>
+#include "esos_pic24.h"
+#include "esos_pic24_rs232.h"
+#include <p33EP512GP806.h>
+#include <pic24_all.h>
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "revF14.h"
+
+ESOS_USER_TASK()
+
+void user_init(void){
+    
+}
