db98938230faa43507fbb12691ed5273e38226f0
AYingst <60374870+AYingst@users.noreply.github.com>
2020-02-23 12:00:10 -0600

    lots more comments

--- BUILD_DIR/t4_sensor1.c
+++ BUILD_DIR/t4_sensor1.c
@@ -18,38 +18,41 @@ ESOS_USER_TASK(loop) {
     static uint16_t u16_data;
 
     ESOS_TASK_BEGIN();{
-      for (;;) {
+      for (;;) {     //same as while(true)
 
-        ESOS_TASK_WAIT_UNTIL(esos_uiF14_isSW1Pressed() || esos_uiF14_isSW2Pressed());
+        ESOS_TASK_WAIT_UNTIL(esos_uiF14_isSW1Pressed() || esos_uiF14_isSW2Pressed());  /on either switch, start the DO loop
 		if (esos_uiF14_isSW2Pressed()){
-			b_keepLooping = TRUE;
+			b_keepLooping = TRUE;   //if sw2 then keep looping; checked at the bottom while statement
 		}
-		else {b_keepLooping = FALSE;
+		else {b_keepLooping = FALSE;   //if sw1 don't keep looping just do once
 		}
-        ESOS_TASK_WAIT_UNTIL(esos_uiF14_isSW1Released() && esos_uiF14_isSW2Released());
-        do{                    //claim the ADC
+        ESOS_TASK_WAIT_UNTIL(esos_uiF14_isSW1Released() && esos_uiF14_isSW2Released());  /*wait for the release so
+	   sw1 can exit the loop when pressed again. The loop is fast enough to go around and sense sw1 still pressed
+	   and begin again*/
+        do{                    //claim the ADC, setup for CH2(potentiometer) and 3V3 for upper Vref
         ESOS_TASK_WAIT_ON_AVAILABLE_SENSOR(ESOS_SENSOR_CH02, ESOS_SENSOR_VREF_3V3);
 
-        //Now we have exclusive use, grab data
+        //do single read of ADC
         ESOS_TASK_WAIT_SENSOR_QUICK_READ(u16_data);
 
-        //Grab the output
+        //wait for UART availability to send output to Bully Bootloader
         ESOS_TASK_WAIT_ON_AVAILABLE_OUT_COMM();
 		
         sprintf(buffer, "%d\n", u16_data);
-        ESOS_TASK_WAIT_ON_SEND_STRING(buffer);
+        ESOS_TASK_WAIT_ON_SEND_STRING(buffer); //wait for data in buffer to be sent and release UART
         ESOS_TASK_SIGNAL_AVAILABLE_OUT_COMM();
-		ESOS_TASK_WAIT_TICKS(LOOP_DELAY /2);
-		if (esos_uiF14_isSW1Pressed()){
+		ESOS_TASK_WAIT_TICKS(LOOP_DELAY /2);  /*this is half of the 1 second delay between samples
+		  the following sw presses could be missed during the full second so we split the delay into 2 parts*/
+		if (esos_uiF14_isSW1Pressed()){  //sw1 will stop the sw2 initiated continous looping
 			ESOS_TASK_WAIT_UNTIL(esos_uiF14_isSW1Released());
 			b_keepLooping = FALSE;
 		}
-		if (esos_uiF14_isSW2Pressed()){
+		if (esos_uiF14_isSW2Pressed()){  //sw2 will also stop the looping
 			ESOS_TASK_WAIT_UNTIL(esos_uiF14_isSW2Released());
 			b_keepLooping = FALSE;
 		}
-		ESOS_TASK_WAIT_TICKS(LOOP_DELAY /2);
-		}while(b_keepLooping);
+		ESOS_TASK_WAIT_TICKS(LOOP_DELAY /2);  //the other half of the loop sw2 sensor reading delay
+		}while(b_keepLooping); //keep doing the DO loop if sw2 initiated it
 		
         //Release the potentiometer
         ESOS_SENSOR_CLOSE();        
