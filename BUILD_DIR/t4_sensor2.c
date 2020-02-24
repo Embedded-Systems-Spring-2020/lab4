702cf3e92eb4cd8a37e572a599cd3ba8efdde8b8
Garrett Smith <ggasmithh@users.noreply.github.com>
2020-02-23 22:14:14 -0600

    Changed menu from loop to one-shot

--- BUILD_DIR/t4_sensor2.c
+++ BUILD_DIR/t4_sensor2.c
@@ -61,93 +61,91 @@ ESOS_CHILD_TASK(menu) {
     char num_samples;
 
     ESOS_TASK_BEGIN();
-    for (;;) {
-       ESOS_TASK_WAIT_ON_AVAILABLE_OUT_COMM();     // wait until we can grab the output stream
-        // print a very pretty menu
-        ESOS_TASK_WAIT_ON_SEND_STRING("\n+===========================+");
-        ESOS_TASK_WAIT_ON_SEND_STRING("|  Select Processing mode   |\n");
-        ESOS_TASK_WAIT_ON_SEND_STRING("|   1. one-shot             |\n");
-        ESOS_TASK_WAIT_ON_SEND_STRING("|   2. average              |\n");
-        ESOS_TASK_WAIT_ON_SEND_STRING("|   3. minimum              |\n");
-        ESOS_TASK_WAIT_ON_SEND_STRING("|   4. maximum              |\n");
-        ESOS_TASK_WAIT_ON_SEND_STRING("|   5. median               |\n");
-        ESOS_TASK_WAIT_ON_SEND_STRING("+===========================+\n\n");
-        ESOS_TASK_WAIT_ON_SEND_STRING("> ");
-        ESOS_TASK_SIGNAL_AVAILABLE_OUT_COMM();      // let everyone else know we are done with the out stream
+    ESOS_TASK_WAIT_ON_AVAILABLE_OUT_COMM();     // wait until we can grab the output stream
+    // print a very pretty menu
+    ESOS_TASK_WAIT_ON_SEND_STRING("\n+===========================+");
+    ESOS_TASK_WAIT_ON_SEND_STRING("|  Select Processing mode   |\n");
+    ESOS_TASK_WAIT_ON_SEND_STRING("|   1. one-shot             |\n");
+    ESOS_TASK_WAIT_ON_SEND_STRING("|   2. average              |\n");
+    ESOS_TASK_WAIT_ON_SEND_STRING("|   3. minimum              |\n");
+    ESOS_TASK_WAIT_ON_SEND_STRING("|   4. maximum              |\n");
+    ESOS_TASK_WAIT_ON_SEND_STRING("|   5. median               |\n");
+    ESOS_TASK_WAIT_ON_SEND_STRING("+===========================+\n\n");
+    ESOS_TASK_WAIT_ON_SEND_STRING("> ");
+    ESOS_TASK_SIGNAL_AVAILABLE_OUT_COMM();      // let everyone else know we are done with the out stream
 
-        ESOS_TASK_WAIT_ON_AVAILABLE_IN_COMM();      // wait until we can grab the input stream
-        ESOS_TASK_WAIT_ON_GET_STRING(proc_mode_buffer);     // write the user's input to the aforementioned buffer
-        ESOS_TASK_SIGNAL_AVAILABLE_IN_COMM();       // let everyone else know we are done with the in stream
+    ESOS_TASK_WAIT_ON_AVAILABLE_IN_COMM();      // wait until we can grab the input stream
+    ESOS_TASK_WAIT_ON_GET_STRING(proc_mode_buffer);     // write the user's input to the aforementioned buffer
+    ESOS_TASK_SIGNAL_AVAILABLE_IN_COMM();       // let everyone else know we are done with the in stream
 
-        // do the above process, but for output (so the user can see what they typed)
-        ESOS_TASK_WAIT_ON_AVAILABLE_OUT_COMM();
-        ESOS_TASK_WAIT_ON_SEND_STRING(proc_mode_buffer);
-        ESOS_TASK_SIGNAL_AVAILABLE_OUT_COMM();
+    // do the above process, but for output (so the user can see what they typed)
+    ESOS_TASK_WAIT_ON_AVAILABLE_OUT_COMM();
+    ESOS_TASK_WAIT_ON_SEND_STRING(proc_mode_buffer);
+    ESOS_TASK_SIGNAL_AVAILABLE_OUT_COMM();
 
-        // start the second part of the menu (largely same as above)
-        ESOS_TASK_WAIT_ON_AVAILABLE_OUT_COMM();
-        ESOS_TASK_WAIT_ON_SEND_STRING("\n+===========================+\n");
-        ESOS_TASK_WAIT_ON_SEND_STRING("|  Select Number of Samples |\n");
-        ESOS_TASK_WAIT_ON_SEND_STRING("|   1. two                  |\n");
-        ESOS_TASK_WAIT_ON_SEND_STRING("|   2. four                 |\n");
-        ESOS_TASK_WAIT_ON_SEND_STRING("|   3. eight                |\n");
-        ESOS_TASK_WAIT_ON_SEND_STRING("|   4. sixteen              |\n");
-        ESOS_TASK_WAIT_ON_SEND_STRING("|   5. thirty-two           |\n");
-        ESOS_TASK_WAIT_ON_SEND_STRING("|   6. sixty-four           |\n");
-        ESOS_TASK_WAIT_ON_SEND_STRING("|   7. one-shot             |\n");
-        ESOS_TASK_WAIT_ON_SEND_STRING("+===========================+\n\n");
-        ESOS_TASK_WAIT_ON_SEND_STRING("> ");
-        ESOS_TASK_SIGNAL_AVAILABLE_OUT_COMM();
+    // start the second part of the menu (largely same as above)
+    ESOS_TASK_WAIT_ON_AVAILABLE_OUT_COMM();
+    ESOS_TASK_WAIT_ON_SEND_STRING("\n+===========================+\n");
+    ESOS_TASK_WAIT_ON_SEND_STRING("|  Select Number of Samples |\n");
+    ESOS_TASK_WAIT_ON_SEND_STRING("|   1. two                  |\n");
+    ESOS_TASK_WAIT_ON_SEND_STRING("|   2. four                 |\n");
+    ESOS_TASK_WAIT_ON_SEND_STRING("|   3. eight                |\n");
+    ESOS_TASK_WAIT_ON_SEND_STRING("|   4. sixteen              |\n");
+    ESOS_TASK_WAIT_ON_SEND_STRING("|   5. thirty-two           |\n");
+    ESOS_TASK_WAIT_ON_SEND_STRING("|   6. sixty-four           |\n");
+    ESOS_TASK_WAIT_ON_SEND_STRING("|   7. one-shot             |\n");
+    ESOS_TASK_WAIT_ON_SEND_STRING("+===========================+\n\n");
+    ESOS_TASK_WAIT_ON_SEND_STRING("> ");
+    ESOS_TASK_SIGNAL_AVAILABLE_OUT_COMM();
 
-        ESOS_TASK_WAIT_ON_AVAILABLE_IN_COMM();
-        ESOS_TASK_WAIT_ON_GET_STRING(num_samples_buffer);
-        ESOS_TASK_SIGNAL_AVAILABLE_IN_COMM();
+    ESOS_TASK_WAIT_ON_AVAILABLE_IN_COMM();
+    ESOS_TASK_WAIT_ON_GET_STRING(num_samples_buffer);
+    ESOS_TASK_SIGNAL_AVAILABLE_IN_COMM();
 
-        ESOS_TASK_WAIT_ON_AVAILABLE_OUT_COMM();
-        ESOS_TASK_WAIT_ON_SEND_STRING(num_samples_buffer);
-        ESOS_TASK_SIGNAL_AVAILABLE_OUT_COMM();
+    ESOS_TASK_WAIT_ON_AVAILABLE_OUT_COMM();
+    ESOS_TASK_WAIT_ON_SEND_STRING(num_samples_buffer);
+    ESOS_TASK_SIGNAL_AVAILABLE_OUT_COMM();
 
-        // we only need the first character of each buffer, so doing this now
-        // will save me a lot of typing
-        proc_mode = proc_mode_buffer[0];
-        num_samples = num_samples_buffer[0];
+    // we only need the first character of each buffer, so doing this now
+    // will save me a lot of typing
+    proc_mode = proc_mode_buffer[0];
+    num_samples = num_samples_buffer[0];
 
-        // assign the appropriate sensor_processing_mode based on user input
-        // in the order defined in esos_sensor.h
-        sensor_processing_mode = 
-                proc_mode == ONESHOT && num_samples == SAMPLES_ONESHOT ? ESOS_SENSOR_ONE_SHOT
+    // assign the appropriate sensor_processing_mode based on user input
+    // in the order defined in esos_sensor.h
+    sensor_processing_mode = 
+            proc_mode == ONESHOT && num_samples == SAMPLES_ONESHOT ? ESOS_SENSOR_ONE_SHOT
 
-            :   proc_mode == AVG && num_samples == SAMPLES2 ? ESOS_SENSOR_AVG2
-            :   proc_mode == AVG && num_samples == SAMPLES4 ? ESOS_SENSOR_AVG4
-            :   proc_mode == AVG && num_samples == SAMPLES8 ? ESOS_SENSOR_AVG8
-            :   proc_mode == AVG && num_samples == SAMPLES16 ? ESOS_SENSOR_AVG16
-            :   proc_mode == AVG && num_samples == SAMPLES32 ? ESOS_SENSOR_AVG32
-            :   proc_mode == AVG && num_samples == SAMPLES64 ? ESOS_SENSOR_AVG64
-            
-            :   proc_mode == MIN && num_samples == SAMPLES2 ? ESOS_SENSOR_MIN2
-            :   proc_mode == MIN && num_samples == SAMPLES4 ? ESOS_SENSOR_MIN4
-            :   proc_mode == MIN && num_samples == SAMPLES8 ? ESOS_SENSOR_MIN8
-            :   proc_mode == MIN && num_samples == SAMPLES16 ? ESOS_SENSOR_MIN16
-            :   proc_mode == MIN && num_samples == SAMPLES32 ? ESOS_SENSOR_MIN32
-            :   proc_mode == MIN && num_samples == SAMPLES64 ? ESOS_SENSOR_MIN16
+        :   proc_mode == AVG && num_samples == SAMPLES2 ? ESOS_SENSOR_AVG2
+        :   proc_mode == AVG && num_samples == SAMPLES4 ? ESOS_SENSOR_AVG4
+        :   proc_mode == AVG && num_samples == SAMPLES8 ? ESOS_SENSOR_AVG8
+        :   proc_mode == AVG && num_samples == SAMPLES16 ? ESOS_SENSOR_AVG16
+        :   proc_mode == AVG && num_samples == SAMPLES32 ? ESOS_SENSOR_AVG32
+        :   proc_mode == AVG && num_samples == SAMPLES64 ? ESOS_SENSOR_AVG64
+        
+        :   proc_mode == MIN && num_samples == SAMPLES2 ? ESOS_SENSOR_MIN2
+        :   proc_mode == MIN && num_samples == SAMPLES4 ? ESOS_SENSOR_MIN4
+        :   proc_mode == MIN && num_samples == SAMPLES8 ? ESOS_SENSOR_MIN8
+        :   proc_mode == MIN && num_samples == SAMPLES16 ? ESOS_SENSOR_MIN16
+        :   proc_mode == MIN && num_samples == SAMPLES32 ? ESOS_SENSOR_MIN32
+        :   proc_mode == MIN && num_samples == SAMPLES64 ? ESOS_SENSOR_MIN16
 
-            :   proc_mode == MAX && num_samples == SAMPLES2 ? ESOS_SENSOR_MAX2
-            :   proc_mode == MAX && num_samples == SAMPLES4 ? ESOS_SENSOR_MAX4
-            :   proc_mode == MAX && num_samples == SAMPLES8 ? ESOS_SENSOR_MAX8
-            :   proc_mode == MAX && num_samples == SAMPLES16 ? ESOS_SENSOR_MAX16
-            :   proc_mode == MAX && num_samples == SAMPLES32 ? ESOS_SENSOR_MAX32
-            :   proc_mode == MAX && num_samples == SAMPLES64 ? ESOS_SENSOR_MAX16
+        :   proc_mode == MAX && num_samples == SAMPLES2 ? ESOS_SENSOR_MAX2
+        :   proc_mode == MAX && num_samples == SAMPLES4 ? ESOS_SENSOR_MAX4
+        :   proc_mode == MAX && num_samples == SAMPLES8 ? ESOS_SENSOR_MAX8
+        :   proc_mode == MAX && num_samples == SAMPLES16 ? ESOS_SENSOR_MAX16
+        :   proc_mode == MAX && num_samples == SAMPLES32 ? ESOS_SENSOR_MAX32
+        :   proc_mode == MAX && num_samples == SAMPLES64 ? ESOS_SENSOR_MAX16
 
-            :   proc_mode == MEDIAN && num_samples == SAMPLES2 ? ESOS_SENSOR_MEDIAN2
-            :   proc_mode == MEDIAN && num_samples == SAMPLES4 ? ESOS_SENSOR_MEDIAN4
-            :   proc_mode == MEDIAN && num_samples == SAMPLES8 ? ESOS_SENSOR_MEDIAN8
-            :   proc_mode == MEDIAN && num_samples == SAMPLES16 ? ESOS_SENSOR_MEDIAN16
-            :   proc_mode == MEDIAN && num_samples == SAMPLES32 ? ESOS_SENSOR_MEDIAN32
-            :   proc_mode == MEDIAN && num_samples == SAMPLES64 ? ESOS_SENSOR_MEDIAN16
-            :   0x00;
+        :   proc_mode == MEDIAN && num_samples == SAMPLES2 ? ESOS_SENSOR_MEDIAN2
+        :   proc_mode == MEDIAN && num_samples == SAMPLES4 ? ESOS_SENSOR_MEDIAN4
+        :   proc_mode == MEDIAN && num_samples == SAMPLES8 ? ESOS_SENSOR_MEDIAN8
+        :   proc_mode == MEDIAN && num_samples == SAMPLES16 ? ESOS_SENSOR_MEDIAN16
+        :   proc_mode == MEDIAN && num_samples == SAMPLES32 ? ESOS_SENSOR_MEDIAN32
+        :   proc_mode == MEDIAN && num_samples == SAMPLES64 ? ESOS_SENSOR_MEDIAN16
+        :   0x00;
 
-        ESOS_TASK_YIELD();
-    }
+    ESOS_TASK_YIELD();
     ESOS_TASK_END();
 }
 
