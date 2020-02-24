acf832bb19d513bfdf2cc43b0f735d860fda2a96
Garrett Smith <ggasmithh@users.noreply.github.com>
2020-02-23 21:24:38 -0600

    Minor changes

--- BUILD_DIR/t4_sensor2.c
+++ BUILD_DIR/t4_sensor2.c
@@ -1,16 +1,17 @@
-#include <p33EP512GP806.h>
-#include <stdio.h>
-#include <stdlib.h>
 #include "pic24_all.h"
-#include <string.h>
+#include <p33EP512GP806.h>
 #include "pic24_ports_config.h"
+
 #include <esos.h>
-#include "esos_pic24.h"
 #include <esos_task.h>
 #include "esos_comm.h"
 #include "esos_f14ui.h"
+#include "esos_pic24.h"
 #include "esos_sensor.h"
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 #define LOOP_DELAY 1000
 #define ONESHOT '1'
@@ -26,11 +27,8 @@
 #define SAMPLES64 '6'
 #define SAMPLES_ONESHOT '7'
 
-
 // defined in "esos_sensor.h"
 static esos_sensor_process_t sensor_processing_mode;
-
-
 
 ESOS_CHILD_TASK(menu) {
 
@@ -138,118 +136,71 @@ ESOS_CHILD_TASK(menu) {
     ESOS_TASK_END();
 }
 
-//ESOS_USER_TASK(placeholderTask) {
-//    ESOS_TASK_HANDLE th_child;
-//
-//    ESOS_TASK_BEGIN();
-//    ESOS_TASK_SPAWN_AND_WAIT(th_child, menu);
-//    ESOS_TASK_END();
-//    
-//}
+
 
 BOOL b_keepLooping = FALSE;
 
 ESOS_USER_TASK(loop) {
 
     static uint16_t u16_data;
+    ESOS_TASK_HANDLE th_child;
 
     ESOS_TASK_BEGIN(); {
-
         for (;;) {
-
-
-
             ESOS_TASK_WAIT_UNTIL(esos_uiF14_isSW1Pressed() || esos_uiF14_isSW2Pressed());
 
             if (esos_uiF14_isSW2Pressed()) {
-
                 b_keepLooping = TRUE;
-
             }
 
             else {
                 b_keepLooping = FALSE;
-
             }
 
             ESOS_TASK_WAIT_UNTIL(esos_uiF14_isSW1Released() && esos_uiF14_isSW2Released());
 
-            do {                    //claim the ADC
-
+            do {                    
+                //claim the ADC
                 ESOS_TASK_WAIT_ON_AVAILABLE_SENSOR(ESOS_SENSOR_CH02, ESOS_SENSOR_VREF_3V3);
 
-
-
                 //Now we have exclusive use, grab data
-
                 ESOS_TASK_WAIT_SENSOR_QUICK_READ(u16_data);
 
-
-
                 //Grab the output
-
                 ESOS_TASK_WAIT_ON_AVAILABLE_OUT_COMM();
                 ESOS_TASK_WAIT_ON_SEND_STRING("Hex: ");
                 ESOS_TASK_WAIT_ON_SEND_UINT32_AS_HEX_STRING(u16_data);
                 ESOS_TASK_WAIT_ON_SEND_STRING("\n");
                 ESOS_TASK_SIGNAL_AVAILABLE_OUT_COMM();
 
-                //  sprintf(buffer, "0x%04X\n", u16_data);
-                 // ESOS_TASK_WAIT_ON_SEND_STRING(buffer);
-                 // ESOS_TASK_SIGNAL_AVAILABLE_OUT_COMM();
-
                 ESOS_TASK_WAIT_TICKS(LOOP_DELAY / 2);
 
                 if (esos_uiF14_isSW1Pressed()) {
-
                     ESOS_TASK_WAIT_UNTIL(esos_uiF14_isSW1Released());
-
                     b_keepLooping = FALSE;
-
                 }
 
                 if (esos_uiF14_isSW2Pressed()) {
-
                     ESOS_TASK_WAIT_UNTIL(esos_uiF14_isSW2Released());
                     b_keepLooping = FALSE;
-
                 }
 
-                if (esos_uiF14_isSW3Pressed()) {
-
-                    //Prompt menu when SW3 is pressed 
-                    ESOS_TASK_WAIT_UNTIL(esos_uiF14_isSW3Released());
+                if (esos_uiF14_isSW3Pressed()){  //sw1 will stop the sw2 initiated continous looping
+			        ESOS_TASK_WAIT_UNTIL(esos_uiF14_isSW3Released());
                     b_keepLooping = FALSE;
-                }
-             //   ESOS_TASK_HANDLE th_child;
-             //   ESOS_TASK_SPAWN_AND_WAIT(th_child, menu);
-
-                ESOS_TASK_WAIT_TICKS(LOOP_DELAY / 2);
-
-            } while (b_keepLooping);
-
-
-
-            //Release the potentiometer
-
-            ESOS_SENSOR_CLOSE();
-        }
-
+                    ESOS_TASK_SPAWN_AND_WAIT(th_child, menu);
+		        }
+		        ESOS_TASK_WAIT_TICKS(LOOP_DELAY /2);  //the other half of the loop sw2 sensor reading delay
+		    }while(b_keepLooping); //keep doing the DO loop if sw2 initiated it
+		
+        //Release the potentiometer
+        ESOS_SENSOR_CLOSE();        
+      }  
     }
-
     ESOS_TASK_END();
-
 }
 
 void user_init(void){
     config_esos_uiF14();
-
-    //config heartbeat LED
-    esos_uiF14_flashLED3(500);
-
     esos_RegisterTask(loop);
-    //esos_RegisterTask(placeholderTask);
-    
-    
-  
 }
