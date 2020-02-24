594eda378e760f619fcede7a1b158b53d225276a
Andrew Yingst <andrewlyingst@gmail.com>
2020-02-24 14:56:00 -0600

    final task3

--- BUILD_DIR/t4_sensor3.c
+++ BUILD_DIR/t4_sensor3.c
@@ -29,43 +29,165 @@
 
 // defined in "esos_sensor.h"
 static esos_sensor_process_t sensor_processing_mode;
+char buffer[30];
+BOOL b_keepLooping = FALSE;
 
-ESOS_USER_TASK(display_output)
-{
+ESOS_CHILD_TASK(barGraph_child, uint8_t u8_num2graph){  //visual display of data
+	
+	ESOS_TASK_BEGIN();
+	 //wait for UART availability to send output to Bully Bootloader
+    ESOS_TASK_WAIT_ON_AVAILABLE_OUT_COMM();
+    ESOS_TASK_WAIT_ON_SEND_UINT8_AS_HEX_STRING(u8_num2graph); 
+	ESOS_TASK_WAIT_ON_SEND_STRING(" C\n");
+	ESOS_TASK_WAIT_ON_SEND_STRING(buffer); //wait for data in buffer to be sent and release UART
+    ESOS_TASK_SIGNAL_AVAILABLE_OUT_COMM();
+	ESOS_TASK_END();
+}
+
+ESOS_CHILD_TASK(menu) {
+    static uint8_t proc_mode = 0x00;
+    static uint8_t num_samples = 0x00;
+
     ESOS_TASK_BEGIN();
-    ESOS_TASK_WAIT_ON_AVAILABLE_SENSOR(ESOS_SENSOR_CH03, ESOS_SENSOR_VREF_3V3);
-    while(TRUE)
-    {
-        static uint32_t pu32_out; //CONVERSION VARIABLE FOR ADC SENSOR DATA
-        static uint32_t temp32_int; //TEMPATURE CONVERSATION VARIABLE
-        static uint32_t temp32_dec; //TEMPATURE FINSIHED PRODUCT VARAIBLE
    
-        //Math logic
-        // Vo = (6.25mV * T) + 424mV
-        // T ~= (pu16_out - 424) / 6
+        ESOS_TASK_WAIT_ON_AVAILABLE_OUT_COMM();     // wait until we can grab the output stream
+        // print a very pretty menu
+        ESOS_TASK_WAIT_ON_SEND_STRING("\n+===========================+");
+        ESOS_TASK_WAIT_ON_SEND_STRING("|  Select Processing mode   |\n");
+        ESOS_TASK_WAIT_ON_SEND_STRING("|   1. one-shot             |\n");
+        ESOS_TASK_WAIT_ON_SEND_STRING("|   2. average              |\n");
+        ESOS_TASK_WAIT_ON_SEND_STRING("|   3. minimum              |\n");
+        ESOS_TASK_WAIT_ON_SEND_STRING("|   4. maximum              |\n");
+        ESOS_TASK_WAIT_ON_SEND_STRING("|   5. median               |\n");
+        ESOS_TASK_WAIT_ON_SEND_STRING("+===========================+\n\n");
+        ESOS_TASK_WAIT_ON_SEND_STRING("> ");
+        ESOS_TASK_SIGNAL_AVAILABLE_OUT_COMM();      // let everyone else know we are done with the out stream
 
-        // pu16_out is still raw ADC sensor data meaning So multiplying it by the top voltage value which 3V and then divide by 1048.
-        pu32_out = (uint32_t)(3*pu16_out/1048) * 1000; // convert to not use decimals
-        pu32_out = (pu32_out - 424000) / 625; // millimillivolts to temp
-        temp32_int = pu32_out / 100; // convert to get the integer part
-        temp32_dec = pu32_out - temp32_int * 100; // subtract out the integer part to get the decimal
-                                                                        // part
-        static char buffer[12];
-        ESOS_TASK_WAIT_ON_AVAILABLE_OUT_COMM();                  
-        sprintf(buffer, "%d\n", temp32_dec); //Print converted ADC Sensor reading  
+        ESOS_TASK_WAIT_ON_AVAILABLE_IN_COMM();      // wait until we can grab the input stream
+        ESOS_TASK_WAIT_ON_GET_UINT8(proc_mode);     // write the user's input to the aforementioned buffer
+        ESOS_TASK_SIGNAL_AVAILABLE_IN_COMM();       // let everyone else know we are done with the in stream
+
+        // start the second part of the menu (largely same as above)
+        ESOS_TASK_WAIT_ON_AVAILABLE_OUT_COMM();
+        ESOS_TASK_WAIT_ON_SEND_STRING("\n+===========================+\n");
+        ESOS_TASK_WAIT_ON_SEND_STRING("|  Select Number of Samples |\n");
+        ESOS_TASK_WAIT_ON_SEND_STRING("|   1. two                  |\n");
+        ESOS_TASK_WAIT_ON_SEND_STRING("|   2. four                 |\n");
+        ESOS_TASK_WAIT_ON_SEND_STRING("|   3. eight                |\n");
+        ESOS_TASK_WAIT_ON_SEND_STRING("|   4. sixteen              |\n");
+        ESOS_TASK_WAIT_ON_SEND_STRING("|   5. thirty-two           |\n");
+        ESOS_TASK_WAIT_ON_SEND_STRING("|   6. sixty-four           |\n");
+        ESOS_TASK_WAIT_ON_SEND_STRING("|   7. one-shot             |\n");
+        ESOS_TASK_WAIT_ON_SEND_STRING("+===========================+\n\n");
+        ESOS_TASK_WAIT_ON_SEND_STRING("> ");
         ESOS_TASK_SIGNAL_AVAILABLE_OUT_COMM();
-        ESOS_TASK_WAIT_TICKS(1000);
-    }
-    
-    ESOS_SENSOR_CLOSE(); //TURN OFF ADC AND SIGNAL
-    
-    ESOS_TASK_YIELD();
+
+        ESOS_TASK_WAIT_ON_AVAILABLE_IN_COMM();
+        ESOS_TASK_WAIT_ON_GET_UINT8(num_samples);
+		
+        ESOS_TASK_SIGNAL_AVAILABLE_IN_COMM();
+
+        // assign the appropriate sensor_processing_mode based on user input
+        // in the order defined in esos_sensor.h
+        sensor_processing_mode = 
+                proc_mode == ONESHOT && num_samples == SAMPLES_ONESHOT ? ESOS_SENSOR_ONE_SHOT
+
+            :   proc_mode == AVG && num_samples == SAMPLES2 ? ESOS_SENSOR_AVG2
+            :   proc_mode == AVG && num_samples == SAMPLES4 ? ESOS_SENSOR_AVG4
+            :   proc_mode == AVG && num_samples == SAMPLES8 ? ESOS_SENSOR_AVG8
+            :   proc_mode == AVG && num_samples == SAMPLES16 ? ESOS_SENSOR_AVG16
+            :   proc_mode == AVG && num_samples == SAMPLES32 ? ESOS_SENSOR_AVG32
+            :   proc_mode == AVG && num_samples == SAMPLES64 ? ESOS_SENSOR_AVG64
+            
+            :   proc_mode == MIN && num_samples == SAMPLES2 ? ESOS_SENSOR_MIN2
+            :   proc_mode == MIN && num_samples == SAMPLES4 ? ESOS_SENSOR_MIN4
+            :   proc_mode == MIN && num_samples == SAMPLES8 ? ESOS_SENSOR_MIN8
+            :   proc_mode == MIN && num_samples == SAMPLES16 ? ESOS_SENSOR_MIN16
+            :   proc_mode == MIN && num_samples == SAMPLES32 ? ESOS_SENSOR_MIN32
+            :   proc_mode == MIN && num_samples == SAMPLES64 ? ESOS_SENSOR_MIN16
+
+            :   proc_mode == MAX && num_samples == SAMPLES2 ? ESOS_SENSOR_MAX2
+            :   proc_mode == MAX && num_samples == SAMPLES4 ? ESOS_SENSOR_MAX4
+            :   proc_mode == MAX && num_samples == SAMPLES8 ? ESOS_SENSOR_MAX8
+            :   proc_mode == MAX && num_samples == SAMPLES16 ? ESOS_SENSOR_MAX16
+            :   proc_mode == MAX && num_samples == SAMPLES32 ? ESOS_SENSOR_MAX32
+            :   proc_mode == MAX && num_samples == SAMPLES64 ? ESOS_SENSOR_MAX16
+
+            :   proc_mode == MEDIAN && num_samples == SAMPLES2 ? ESOS_SENSOR_MEDIAN2
+            :   proc_mode == MEDIAN && num_samples == SAMPLES4 ? ESOS_SENSOR_MEDIAN4
+            :   proc_mode == MEDIAN && num_samples == SAMPLES8 ? ESOS_SENSOR_MEDIAN8
+            :   proc_mode == MEDIAN && num_samples == SAMPLES16 ? ESOS_SENSOR_MEDIAN16
+            :   proc_mode == MEDIAN && num_samples == SAMPLES32 ? ESOS_SENSOR_MEDIAN32
+            :   proc_mode == MEDIAN && num_samples == SAMPLES64 ? ESOS_SENSOR_MEDIAN16
+            :   0x00;
+  	ESOS_TASK_END();
+}
+
+ESOS_USER_TASK(loop) {
+	static uint8_t u8_temp_data;
+    static uint16_t u16_data;
+	static uint64_t u64_temp_data;
+	static ESOS_TASK_HANDLE th_child_bar_graph; //declare storage for handle to child task
+    static ESOS_TASK_HANDLE th_child_menu;
+    ESOS_TASK_BEGIN();
+    for (;;) {     //same as while(true)
+
+        ESOS_TASK_WAIT_UNTIL(esos_uiF14_isSW1Pressed() || esos_uiF14_isSW2Pressed() || \
+							esos_uiF14_isSW3Pressed());  //on any switch, start the sample DO loop
+        if (esos_uiF14_isSW2Pressed()){
+            b_keepLooping = TRUE;   //if sw2 then keep looping; checked at the bottom while statement
+        }
+        else {b_keepLooping = FALSE;   //if sw1 don't keep looping just do once
+        }
+		sensor_processing_mode = ESOS_SENSOR_ONE_SHOT; //for SW1 and SW2 runs
+		if (esos_uiF14_isSW3Pressed()){
+			start_menu:  //this is used lower in the loop if SW3 stops a SW2 continuous sample
+            ESOS_ALLOCATE_CHILD_TASK(th_child_menu);
+            ESOS_TASK_SPAWN_AND_WAIT(th_child_menu, menu);
+        }
+        ESOS_TASK_WAIT_UNTIL(esos_uiF14_isSW1Released() && esos_uiF14_isSW2Released());  /*wait for the release so
+        sw1 can exit the loop when pressed again. The loop is fast enough to go around and sense sw1 still pressed
+        and begin again*/
+        do{         //claim the ADC, setup for CH2(potentiometer) and 3V3 for upper Vref
+            ESOS_TASK_WAIT_ON_AVAILABLE_SENSOR(ESOS_SENSOR_CH03, ESOS_SENSOR_VREF_3V0);
+
+            //do single read of ADC
+            ESOS_TASK_WAIT_SENSOR_READ(u16_data, sensor_processing_mode, ESOS_SENSOR_FORMAT_BITS);
+            //divides ADC return by 2^12 max value and uses equation from datasheet but x100 to avoid decimals
+			u64_temp_data = (uint64_t)(((300000 * u16_data/4096) - 42400) / 625);
+			u8_temp_data = (uint8_t)u64_temp_data;		//forces format smaller for readability	
+			
+			//ouput the data to the screen
+            ESOS_ALLOCATE_CHILD_TASK(th_child_bar_graph);
+            ESOS_TASK_SPAWN_AND_WAIT(th_child_bar_graph, barGraph_child, u8_temp_data);
+                        
+            ESOS_TASK_WAIT_TICKS(LOOP_DELAY /2);  /*this is half of the 1 second delay between samples
+            the following sw presses could be missed during the full second so we split the delay into 2 parts*/
+            if (esos_uiF14_isSW1Pressed()){  //sw1 will stop the sw2 initiated continous looping
+                ESOS_TASK_WAIT_UNTIL(esos_uiF14_isSW1Released());
+                b_keepLooping = FALSE;
+            }
+            if (esos_uiF14_isSW2Pressed()){  //sw2 will also stop the looping
+                ESOS_TASK_WAIT_UNTIL(esos_uiF14_isSW2Released());
+                b_keepLooping = FALSE;
+            }
+            if (esos_uiF14_isSW3Pressed()){  //sw3 will also stop the looping
+                ESOS_TASK_WAIT_UNTIL(esos_uiF14_isSW3Released());
+                b_keepLooping = FALSE;
+				goto start_menu;
+                
+            }
+			ESOS_TASK_WAIT_TICKS(LOOP_DELAY /2);  //the other half of the loop sw2 sensor reading delay
+        }while(b_keepLooping); //keep doing the DO loop if sw2 initiated it
+        
+        //Release the potentiometer
+        ESOS_SENSOR_CLOSE();        
+    }  
     ESOS_TASK_END();
 }
 
-void user_init(void)
-{
+void user_init(void){
+	//esos_uif14_flashLED3(250);
     config_esos_uiF14();
-    esos_RegisterTask(menu);
-    esos_RegisterTask(display_output);
+    esos_RegisterTask(loop);
 }
